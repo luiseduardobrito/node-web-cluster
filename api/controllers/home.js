6aebd3d9098e56c167e465837ca962f8ff79d4cf
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-10-28 01:38:47 -0200

    deleted home methods

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -12,49 +12,5 @@ module.exports = {
 			title: lang.home.title,
 			message: lang.home.message
 		});
-	},
-
-	login: function(req, res) {
-
-		response(res).view("home/login", {
-			title: lang.home.title,
-			message: lang.home.message,
-			destination: req.param("destination") || "dashboard"
-		});
-	},
-
-	signup: function(req, res) {
-
-		response(res).view("home/signup", {
-			title: lang.home.title,
-			message: lang.home.message,
-			destination: req.param("destination") || "dashboard"
-		});
-	},	
-
-	dashboard: function(req, res) {
-
-		policy(req, res).check(["logged_in"], function() { 
-		
-			var _user = model.find("user",  {
-
-				_id: req.cookies.user_id
-
-			}, function(results) {
-
-				if(!results[0])
-					response(res).redirect("/login")
-
-				var u = results[0];
-
-				response(res).view("home/dashboard", {
-
-					title: "node-web-cluster",
-					message: lang.home.message,
-					user: u
-				});
-
-			});
-		});		
 	}
 }
